5ad65865b1ad6f680d4e9a9fb14d036e89b2e13f
Pooja Bhat <poojabhat@Poojas-MacBook-Pro.local>
2022-06-21 18:08:58 -0700

    Cleanup

--- src/main/java/com/tecton/client/request/AbstractTectonRequest.java
+++ src/main/java/com/tecton/client/request/AbstractTectonRequest.java
@@ -4,34 +4,28 @@ import com.tecton.client.transport.TectonHttpClient.HttpMethod;
 
 public abstract class AbstractTectonRequest {
 
-  private final transient String endpoint;
-  private final transient HttpMethod method;
+    private final String endpoint;
+    private final HttpMethod method;
+    private String workspaceName;
+    private String featureServiceName;
 
-  private String workspaceName;
 
-  private String featureServiceName;
+    public AbstractTectonRequest(String endpoint, HttpMethod method, String workspaceName, String featureServiceName) {
+        this.endpoint = endpoint;
+        this.method = method;
+        this.workspaceName = workspaceName;
+        this.featureServiceName = featureServiceName;
+    }
 
-  public AbstractTectonRequest(
-      String endpoint, HttpMethod method, String workspaceName, String featureServiceName) {
-    this.endpoint = endpoint;
-    this.method = method;
-    this.workspaceName = workspaceName;
-    this.featureServiceName = featureServiceName;
-  }
+    public String getEndpoint() {
+        return endpoint;
+    }
 
-  public String getEndpoint() {
-    return endpoint;
-  }
+    public HttpMethod getMethod() {
+        return method;
+    }
 
-  public HttpMethod getMethod() {
-    return method;
-  }
 
-  public String getWorkspaceName() {
-    return workspaceName;
-  }
 
-  public String getFeatureServiceName() {
-    return featureServiceName;
-  }
+
 }
