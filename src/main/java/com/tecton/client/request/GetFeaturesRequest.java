0657d3e2c8efa0f0b680e066ec62c08a7ba31bff
Pooja Bhat <poojabhat@Poojas-MacBook-Pro.local>
2022-07-11 12:43:20 -0700

    Fix formatting

--- src/main/java/com/tecton/client/request/GetFeaturesRequest.java
+++ src/main/java/com/tecton/client/request/GetFeaturesRequest.java
@@ -5,10 +5,10 @@ import com.squareup.moshi.Moshi;
 import com.tecton.client.exceptions.TectonClientException;
 import com.tecton.client.exceptions.TectonErrorMessage;
 import com.tecton.client.transport.TectonHttpClient.HttpMethod;
-import org.apache.commons.lang3.StringUtils;
-
 import java.util.*;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
 
 public class GetFeaturesRequest extends AbstractTectonRequest {
   private static final String ENDPOINT = "/api/v1/feature-service/get-features";
@@ -18,52 +18,41 @@ public class GetFeaturesRequest extends AbstractTectonRequest {
       EnumSet.of(MetadataOption.NAME, MetadataOption.DATA_TYPE);
 
   private JsonAdapter<GetFeaturesRequestJson> jsonAdapter;
-  private final GetFeaturesRequestData getFeaturesRequestData;
+  private Map<String, String> joinKeyMap;
+  private Map<String, Object> requestContextMap;
   private Set<MetadataOption> metadataOptions;
 
-  public GetFeaturesRequest(
-      String workspaceName,
-      String featureServiceName,
-      GetFeaturesRequestData getFeaturesRequestData) {
+  GetFeaturesRequest(String workspaceName, String featureServiceName) {
     super(ENDPOINT, httpMethod, workspaceName, featureServiceName);
-    validateRequestParameters(getFeaturesRequestData);
-    this.getFeaturesRequestData = getFeaturesRequestData;
+    this.joinKeyMap = new HashMap<>();
+    this.requestContextMap = new HashMap<>();
     this.metadataOptions = defaultMetadataOptions;
     Moshi moshi = new Moshi.Builder().build();
     jsonAdapter = moshi.adapter(GetFeaturesRequestJson.class);
   }
 
-  public GetFeaturesRequest(
-      String workspaceName,
-      String featureServiceName,
-      GetFeaturesRequestData getFeaturesRequestData,
-      MetadataOption... metadataOptions) {
-
-    super(ENDPOINT, httpMethod, workspaceName, featureServiceName);
-    validateRequestParameters(getFeaturesRequestData);
-    this.getFeaturesRequestData = getFeaturesRequestData;
-
-    List<MetadataOption> metadataOptionList = Arrays.asList(metadataOptions);
-    if (metadataOptionList.contains(MetadataOption.ALL)) {
-      // Add everything except ALL and NONE from MetadataOption EnumSet
-      this.metadataOptions =
-          EnumSet.complementOf(EnumSet.of(MetadataOption.ALL, MetadataOption.NONE));
-    } else if (metadataOptionList.contains(MetadataOption.NONE)) {
-      this.metadataOptions = EnumSet.noneOf(MetadataOption.class);
-    } else {
-      this.metadataOptions = EnumSet.copyOf(metadataOptionList);
-    }
-    this.metadataOptions.addAll(defaultMetadataOptions); // default metadata options
-    Moshi moshi = new Moshi.Builder().build();
-    jsonAdapter = moshi.adapter(GetFeaturesRequestJson.class);
-  }
-
-  GetFeaturesRequestData getFeaturesRequestData() {
-    return this.getFeaturesRequestData;
-  }
-
   Set<MetadataOption> getMetadataOptions() {
     return this.metadataOptions;
+  }
+
+  void setJoinKeyMap(Map<String, String> joinKeyMap) {
+    this.joinKeyMap = joinKeyMap;
+  }
+
+  void setRequestContextMap(Map<String, Object> requestContextMap) {
+    this.requestContextMap = requestContextMap;
+  }
+
+  void setMetadataOptions(Set<MetadataOption> metadataOptions) {
+    this.metadataOptions = metadataOptions;
+  }
+
+  Map<String, String> getJoinKeyMap() {
+    return this.joinKeyMap;
+  }
+
+  Map<String, Object> getRequestContextMap() {
+    return this.requestContextMap;
   }
 
   static class GetFeaturesRequestJson {
@@ -87,11 +76,11 @@ public class GetFeaturesRequest extends AbstractTectonRequest {
     GetFeaturesFields getFeaturesFields = new GetFeaturesFields();
     getFeaturesFields.feature_service_name = this.getFeatureServiceName();
     getFeaturesFields.workspace_name = this.getWorkspaceName();
-    if (!getFeaturesRequestData().isEmptyJoinKeyMap()) {
-      getFeaturesFields.join_key_map = getFeaturesRequestData().getJoinKeyMap();
+    if (!this.joinKeyMap.isEmpty()) {
+      getFeaturesFields.join_key_map = joinKeyMap;
     }
-    if (!getFeaturesRequestData().isEmptyRequestContextMap()) {
-      getFeaturesFields.request_context_map = getFeaturesRequestData().getRequestContextMap();
+    if (!this.requestContextMap.isEmpty()) {
+      getFeaturesFields.request_context_map = requestContextMap;
     }
     if (!metadataOptions.isEmpty()) {
       getFeaturesFields.metadata_options =
@@ -130,10 +119,121 @@ public class GetFeaturesRequest extends AbstractTectonRequest {
     }
   }
 
-  private void validateRequestParameters(GetFeaturesRequestData getFeaturesRequestData) {
-    if (getFeaturesRequestData.isEmptyJoinKeyMap()
-        && getFeaturesRequestData.isEmptyRequestContextMap()) {
-      throw new TectonClientException(TectonErrorMessage.EMPTY_REQUEST_MAPS);
+  public static class Builder {
+    private String workspaceName;
+    private String featureServiceName;
+    private Map<String, String> joinKeyMap;
+    private Map<String, Object> requestContextMap;
+    List<MetadataOption> metadataOptionList;
+
+    public Builder() {
+      this.joinKeyMap = new HashMap<>();
+      this.requestContextMap = new HashMap<>();
+      this.metadataOptionList = new ArrayList<>();
+    }
+
+    public Builder workspaceName(String workspaceName) {
+      this.workspaceName = workspaceName;
+      return this;
+    }
+
+    public Builder featureServiceName(String featureServiceName) {
+      this.featureServiceName = featureServiceName;
+      return this;
+    }
+
+    public Builder joinKeyMap(Map<String, String> joinKeyMap) {
+      this.joinKeyMap = joinKeyMap;
+      return this;
+    }
+
+    public Builder requestContextMap(Map<String, Object> requestContextMap) {
+      this.requestContextMap = requestContextMap;
+      return this;
+    }
+
+    public Builder joinKey(String key, String value) {
+      joinKeyMap.put(key, value);
+      return this;
+    }
+
+    public Builder joinKey(String key, Long value) {
+      joinKeyMap.put(key, value.toString());
+      return this;
+    }
+
+    public Builder requestContext(String key, String value) {
+      requestContextMap.put(key, value);
+      return this;
+    }
+
+    public Builder requestContext(String key, Long value) {
+      requestContextMap.put(key, value.toString());
+      return this;
+    }
+
+    public Builder requestContext(String key, Double value) {
+      requestContextMap.put(key, value);
+      return this;
+    }
+
+    public Builder metadataOptions(MetadataOption... metadataOptions) {
+      this.metadataOptionList = Arrays.asList(metadataOptions);
+      return this;
+    }
+
+    public GetFeaturesRequest build() throws TectonClientException {
+
+      GetFeaturesRequest getFeaturesRequest =
+          new GetFeaturesRequest(workspaceName, featureServiceName);
+      validateRequestParameters();
+      if (!this.joinKeyMap.isEmpty()) {
+        getFeaturesRequest.setJoinKeyMap(joinKeyMap);
+      }
+      if (!this.requestContextMap.isEmpty()) {
+        getFeaturesRequest.setRequestContextMap(requestContextMap);
+      }
+
+      if (this.metadataOptionList.isEmpty()) {
+        getFeaturesRequest.setMetadataOptions(defaultMetadataOptions);
+      } else {
+        Set<MetadataOption> metadataOptions;
+        if (this.metadataOptionList.contains(MetadataOption.ALL)) {
+          // Add everything except ALL and NONE from MetadataOption EnumSet
+          metadataOptions =
+              EnumSet.complementOf(EnumSet.of(MetadataOption.ALL, MetadataOption.NONE));
+        } else if (metadataOptionList.contains(MetadataOption.NONE)) {
+          metadataOptions = EnumSet.noneOf(MetadataOption.class);
+        } else {
+          metadataOptions = EnumSet.copyOf(metadataOptionList);
+        }
+        metadataOptions.addAll(defaultMetadataOptions); // default metadata options
+        getFeaturesRequest.setMetadataOptions(metadataOptions);
+      }
+      return getFeaturesRequest;
+    }
+
+    private void validateRequestParameters() {
+      if (this.joinKeyMap.isEmpty() && this.requestContextMap.isEmpty()) {
+        throw new TectonClientException(TectonErrorMessage.EMPTY_REQUEST_MAPS);
+      }
+      if (!this.joinKeyMap.isEmpty()) {
+        joinKeyMap.forEach(
+            (key, value) -> {
+              Validate.notEmpty(key, TectonErrorMessage.INVALID_KEY_VALUE);
+              Validate.notEmpty(value, TectonErrorMessage.INVALID_KEY_VALUE);
+            });
+      }
+      if (!this.requestContextMap.isEmpty()) {
+        requestContextMap.forEach(
+            (key, value) -> {
+              Validate.notEmpty(key, TectonErrorMessage.INVALID_KEY_VALUE);
+              Validate.notNull(value, TectonErrorMessage.INVALID_KEY_VALUE);
+              if (value instanceof String) {
+                Validate.notEmpty((String) value, TectonErrorMessage.INVALID_KEY_VALUE);
+              }
+            });
+      }
     }
   }
 }
--- src/test/java/com/tecton/client/request/GetFeaturesRequestTest.java
+++ src/test/java/com/tecton/client/request/GetFeaturesRequestTest.java
@@ -1,17 +1,16 @@
 package com.tecton.client.request;
 
+import static org.junit.Assert.fail;
+
 import com.tecton.client.exceptions.TectonClientException;
 import com.tecton.client.exceptions.TectonErrorMessage;
-import com.tecton.client.transport.TectonHttpClient;
 import com.tecton.client.request.GetFeaturesRequest.MetadataOption;
+import com.tecton.client.transport.TectonHttpClient;
+import java.io.IOException;
+import java.util.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.io.IOException;
-import java.util.*;
-
-import static org.junit.Assert.fail;
 
 public class GetFeaturesRequestTest {
 
@@ -22,13 +21,10 @@ public class GetFeaturesRequestTest {
       EnumSet.of(MetadataOption.NAME, MetadataOption.DATA_TYPE);
 
   GetFeaturesRequest getFeaturesRequest;
-  GetFeaturesRequestData defaultFeatureRequestData;
   ClassLoader classLoader;
 
   @Before
   public void setup() {
-    defaultFeatureRequestData = new GetFeaturesRequestData();
-    defaultFeatureRequestData.addJoinKey("testKey", "testValue");
     classLoader = getClass().getClassLoader();
   }
 
@@ -36,7 +32,10 @@ public class GetFeaturesRequestTest {
   public void testEmptyWorkspaceName() {
     try {
       getFeaturesRequest =
-          new GetFeaturesRequest("", TEST_FEATURESERVICE_NAME, defaultFeatureRequestData);
+          new GetFeaturesRequest.Builder()
+              .workspaceName("")
+              .featureServiceName(TEST_FEATURESERVICE_NAME)
+              .build();
       fail();
     } catch (IllegalArgumentException e) {
       Assert.assertEquals(TectonErrorMessage.INVALID_WORKSPACENAME, e.getMessage());
@@ -47,7 +46,10 @@ public class GetFeaturesRequestTest {
   public void testEmptyFeatureServiceName() {
     try {
       getFeaturesRequest =
-          new GetFeaturesRequest(TEST_WORKSPACENAME, "", defaultFeatureRequestData);
+          new GetFeaturesRequest.Builder()
+              .workspaceName(TEST_WORKSPACENAME)
+              .featureServiceName("")
+              .build();
       fail();
     } catch (IllegalArgumentException e) {
       Assert.assertEquals(TectonErrorMessage.INVALID_FEATURESERVICENAME, e.getMessage());
@@ -58,7 +60,10 @@ public class GetFeaturesRequestTest {
   public void testNullWorkspaceName() {
     try {
       getFeaturesRequest =
-          new GetFeaturesRequest(null, TEST_FEATURESERVICE_NAME, defaultFeatureRequestData);
+          new GetFeaturesRequest.Builder()
+              .workspaceName(null)
+              .featureServiceName(TEST_FEATURESERVICE_NAME)
+              .build();
       fail();
     } catch (NullPointerException e) {
       Assert.assertEquals(TectonErrorMessage.INVALID_WORKSPACENAME, e.getMessage());
@@ -69,7 +74,10 @@ public class GetFeaturesRequestTest {
   public void testNullFeatureServiceName() {
     try {
       getFeaturesRequest =
-          new GetFeaturesRequest(TEST_WORKSPACENAME, null, defaultFeatureRequestData);
+          new GetFeaturesRequest.Builder()
+              .workspaceName(TEST_WORKSPACENAME)
+              .featureServiceName(null)
+              .build();
       fail();
     } catch (NullPointerException e) {
       Assert.assertEquals(TectonErrorMessage.INVALID_FEATURESERVICENAME, e.getMessage());
@@ -78,11 +86,14 @@ public class GetFeaturesRequestTest {
 
   @Test
   public void testEmptyMaps() {
-    GetFeaturesRequestData getFeaturesRequestData = new GetFeaturesRequestData();
     try {
       getFeaturesRequest =
-          new GetFeaturesRequest(
-              TEST_WORKSPACENAME, TEST_FEATURESERVICE_NAME, getFeaturesRequestData);
+          new GetFeaturesRequest.Builder()
+              .workspaceName(TEST_WORKSPACENAME)
+              .featureServiceName(TEST_FEATURESERVICE_NAME)
+              .joinKeyMap(new HashMap<>())
+              .requestContextMap(new HashMap<>())
+              .build();
       fail();
     } catch (TectonClientException e) {
       Assert.assertEquals(TectonErrorMessage.EMPTY_REQUEST_MAPS, e.getMessage());
@@ -90,19 +101,222 @@ public class GetFeaturesRequestTest {
   }
 
   @Test
+  public void testNullJoinKey() {
+    try {
+      getFeaturesRequest =
+          new GetFeaturesRequest.Builder()
+              .workspaceName(TEST_WORKSPACENAME)
+              .featureServiceName(TEST_FEATURESERVICE_NAME)
+              .joinKey(null, "testValue")
+              .build();
+      fail();
+    } catch (NullPointerException e) {
+      Assert.assertEquals(TectonErrorMessage.INVALID_KEY_VALUE, e.getMessage());
+    }
+  }
+
+  @Test
+  public void testNullJoinValue() {
+    try {
+      getFeaturesRequest =
+          new GetFeaturesRequest.Builder()
+              .workspaceName(TEST_WORKSPACENAME)
+              .featureServiceName(TEST_FEATURESERVICE_NAME)
+              .joinKey("testKey", (String) null)
+              .build();
+      fail();
+    } catch (NullPointerException e) {
+      Assert.assertEquals(TectonErrorMessage.INVALID_KEY_VALUE, e.getMessage());
+    }
+  }
+
+  @Test
+  public void testNullRequestContextKey() {
+    try {
+      getFeaturesRequest =
+          new GetFeaturesRequest.Builder()
+              .workspaceName(TEST_WORKSPACENAME)
+              .featureServiceName(TEST_FEATURESERVICE_NAME)
+              .requestContext(null, "testValue")
+              .build();
+      fail();
+    } catch (NullPointerException e) {
+      Assert.assertEquals(TectonErrorMessage.INVALID_KEY_VALUE, e.getMessage());
+    }
+  }
+
+  @Test
+  public void testNullRequestContextValue() {
+    try {
+      getFeaturesRequest =
+          new GetFeaturesRequest.Builder()
+              .workspaceName(TEST_WORKSPACENAME)
+              .featureServiceName(TEST_FEATURESERVICE_NAME)
+              .requestContext("testKey", (String) null)
+              .build();
+      fail();
+    } catch (NullPointerException e) {
+      Assert.assertEquals(TectonErrorMessage.INVALID_KEY_VALUE, e.getMessage());
+    }
+  }
+
+  @Test
+  public void testEmptyJoinKey() {
+    try {
+      getFeaturesRequest =
+          new GetFeaturesRequest.Builder()
+              .workspaceName(TEST_WORKSPACENAME)
+              .featureServiceName(TEST_FEATURESERVICE_NAME)
+              .joinKey("", "testValue")
+              .build();
+      fail();
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals(TectonErrorMessage.INVALID_KEY_VALUE, e.getMessage());
+    }
+  }
+
+  @Test
+  public void testEmptyJoinValue() {
+    try {
+      getFeaturesRequest =
+          new GetFeaturesRequest.Builder()
+              .workspaceName(TEST_WORKSPACENAME)
+              .featureServiceName(TEST_FEATURESERVICE_NAME)
+              .joinKey("testKey", "")
+              .build();
+      fail();
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals(TectonErrorMessage.INVALID_KEY_VALUE, e.getMessage());
+    }
+  }
+
+  @Test
+  public void testEmptyRequestContextKey() {
+    try {
+      getFeaturesRequest =
+          new GetFeaturesRequest.Builder()
+              .workspaceName(TEST_WORKSPACENAME)
+              .featureServiceName(TEST_FEATURESERVICE_NAME)
+              .requestContext("", "testValue")
+              .build();
+      fail();
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals(TectonErrorMessage.INVALID_KEY_VALUE, e.getMessage());
+    }
+  }
+
+  @Test
+  public void testEmptyRequestContextValue() {
+    try {
+      getFeaturesRequest =
+          new GetFeaturesRequest.Builder()
+              .workspaceName(TEST_WORKSPACENAME)
+              .featureServiceName(TEST_FEATURESERVICE_NAME)
+              .requestContext("testKey", "")
+              .build();
+      fail();
+    } catch (IllegalArgumentException e) {
+      Assert.assertEquals(TectonErrorMessage.INVALID_KEY_VALUE, e.getMessage());
+    }
+  }
+
+  @Test
+  public void testMixedTypeJoinKeyValues() {
+    getFeaturesRequest =
+        new GetFeaturesRequest.Builder()
+            .workspaceName(TEST_WORKSPACENAME)
+            .featureServiceName(TEST_FEATURESERVICE_NAME)
+            .joinKey("testStringKey", "testValue")
+            .joinKey("testLongKey", 1234L)
+            .build();
+
+    Map<String, String> joinKeyMap = getFeaturesRequest.getJoinKeyMap();
+    Assert.assertNotNull(joinKeyMap);
+    Assert.assertEquals(2, joinKeyMap.size());
+    Assert.assertEquals(joinKeyMap.get("testStringKey"), "testValue");
+    Assert.assertEquals(joinKeyMap.get("testLongKey"), "1234");
+  }
+
+  @Test
+  public void testMixedTypeRequestContextKeyValues() {
+    getFeaturesRequest =
+        new GetFeaturesRequest.Builder()
+            .workspaceName(TEST_WORKSPACENAME)
+            .featureServiceName(TEST_FEATURESERVICE_NAME)
+            .requestContext("testStringKey", "testStringValue")
+            .requestContext("testLongKey", 1234L)
+            .requestContext("testDoubleKey", 125.55)
+            .build();
+
+    Map<String, Object> requestContextMap = getFeaturesRequest.getRequestContextMap();
+    Assert.assertNotNull(requestContextMap);
+    Assert.assertEquals(3, requestContextMap.size());
+    Assert.assertEquals(requestContextMap.get("testStringKey"), "testStringValue");
+    Assert.assertEquals(requestContextMap.get("testLongKey"), "1234");
+    Assert.assertEquals(requestContextMap.get("testDoubleKey"), 125.55);
+  }
+
+  @Test
+  public void testJoinKeyAndRequestContext() {
+    Map<String, String> joinKeyMap =
+        new HashMap<String, String>() {
+          {
+            put("testJoinKey1", "testJoinValue1");
+            put("testJoinKey2", "testJoinValue2");
+          }
+        };
+
+    Map<String, Object> requestContextMap =
+        new HashMap<String, Object>() {
+          {
+            put("testRequestContext1", 555.55);
+            put("testRequestContext2", "testStringValue");
+          }
+        };
+
+    getFeaturesRequest =
+        new GetFeaturesRequest.Builder()
+            .workspaceName(TEST_WORKSPACENAME)
+            .featureServiceName(TEST_FEATURESERVICE_NAME)
+            .joinKeyMap(joinKeyMap)
+            .requestContextMap(requestContextMap)
+            .build();
+
+    Assert.assertEquals(2, getFeaturesRequest.getRequestContextMap().size());
+    Assert.assertEquals(2, getFeaturesRequest.getJoinKeyMap().size());
+    joinKeyMap
+        .keySet()
+        .forEach(
+            key -> {
+              Assert.assertEquals(joinKeyMap.get(key), getFeaturesRequest.getJoinKeyMap().get(key));
+            });
+
+    requestContextMap
+        .keySet()
+        .forEach(
+            key -> {
+              Assert.assertEquals(
+                  requestContextMap.get(key), getFeaturesRequest.getRequestContextMap().get(key));
+            });
+  }
+
+  @Test
   public void testSimpleRequest() {
     getFeaturesRequest =
-        new GetFeaturesRequest(
-            TEST_WORKSPACENAME, TEST_FEATURESERVICE_NAME, defaultFeatureRequestData);
+        new GetFeaturesRequest.Builder()
+            .workspaceName(TEST_WORKSPACENAME)
+            .featureServiceName(TEST_FEATURESERVICE_NAME)
+            .joinKey("testKey", "testValue")
+            .build();
 
     Assert.assertEquals(ENDPOINT, getFeaturesRequest.getEndpoint());
     Assert.assertEquals(TectonHttpClient.HttpMethod.POST, getFeaturesRequest.getMethod());
     Assert.assertEquals(TEST_WORKSPACENAME, getFeaturesRequest.getWorkspaceName());
     Assert.assertEquals(TEST_FEATURESERVICE_NAME, getFeaturesRequest.getFeatureServiceName());
-    Assert.assertTrue(getFeaturesRequest.getFeaturesRequestData().isEmptyRequestContextMap());
+    Assert.assertTrue(getFeaturesRequest.getRequestContextMap().isEmpty());
     Assert.assertEquals(defaultMetadataOptions, getFeaturesRequest.getMetadataOptions());
 
-    Map<String, String> joinKeyMap = getFeaturesRequest.getFeaturesRequestData().getJoinKeyMap();
+    Map<String, String> joinKeyMap = getFeaturesRequest.getJoinKeyMap();
     Assert.assertEquals(1, joinKeyMap.size());
     Assert.assertEquals("testValue", joinKeyMap.get("testKey"));
 
@@ -115,20 +329,23 @@ public class GetFeaturesRequestTest {
 
   @Test
   public void testRequestWithRequestContextMap() throws IOException {
-    defaultFeatureRequestData.addRequestContext("testKey1", 999.999);
-    defaultFeatureRequestData.addRequestContext("testKey2", "testVal");
 
     getFeaturesRequest =
-        new GetFeaturesRequest(
-            TEST_WORKSPACENAME, TEST_FEATURESERVICE_NAME, defaultFeatureRequestData);
+        new GetFeaturesRequest.Builder()
+            .workspaceName(TEST_WORKSPACENAME)
+            .featureServiceName(TEST_FEATURESERVICE_NAME)
+            .joinKey("testKey", "testValue")
+            .requestContext("testKey1", 999.999)
+            .requestContext("testKey2", "testVal")
+            .build();
+
     Assert.assertEquals(ENDPOINT, getFeaturesRequest.getEndpoint());
     Assert.assertEquals(TectonHttpClient.HttpMethod.POST, getFeaturesRequest.getMethod());
     Assert.assertEquals(TEST_WORKSPACENAME, getFeaturesRequest.getWorkspaceName());
     Assert.assertEquals(TEST_FEATURESERVICE_NAME, getFeaturesRequest.getFeatureServiceName());
     Assert.assertEquals(defaultMetadataOptions, getFeaturesRequest.getMetadataOptions());
 
-    Map<String, Object> requestContextMap =
-        getFeaturesRequest.getFeaturesRequestData().getRequestContextMap();
+    Map<String, Object> requestContextMap = getFeaturesRequest.getRequestContextMap();
     Assert.assertEquals(2, requestContextMap.size());
     Assert.assertEquals(999.999, requestContextMap.get("testKey1"));
     Assert.assertEquals("testVal", requestContextMap.get("testKey2"));
@@ -143,11 +360,12 @@ public class GetFeaturesRequestTest {
   @Test
   public void testAllMetadataOptions() {
     getFeaturesRequest =
-        new GetFeaturesRequest(
-            TEST_WORKSPACENAME,
-            TEST_FEATURESERVICE_NAME,
-            defaultFeatureRequestData,
-            MetadataOption.ALL);
+        new GetFeaturesRequest.Builder()
+            .workspaceName(TEST_WORKSPACENAME)
+            .featureServiceName(TEST_FEATURESERVICE_NAME)
+            .joinKey("testKey", "testValue")
+            .metadataOptions(MetadataOption.ALL)
+            .build();
     Assert.assertEquals(4, getFeaturesRequest.getMetadataOptions().size());
     Set<MetadataOption> metadataOptionSet = getFeaturesRequest.getMetadataOptions();
     Set<MetadataOption> expectedSet =
@@ -163,12 +381,12 @@ public class GetFeaturesRequestTest {
   @Test
   public void testCustomMetadataOptions() {
     getFeaturesRequest =
-        new GetFeaturesRequest(
-            TEST_WORKSPACENAME,
-            TEST_FEATURESERVICE_NAME,
-            defaultFeatureRequestData,
-            MetadataOption.DATA_TYPE,
-            MetadataOption.NAME);
+        new GetFeaturesRequest.Builder()
+            .workspaceName(TEST_WORKSPACENAME)
+            .featureServiceName(TEST_FEATURESERVICE_NAME)
+            .joinKey("testKey", "testValue")
+            .metadataOptions(MetadataOption.NAME, MetadataOption.DATA_TYPE)
+            .build();
     Assert.assertEquals(2, getFeaturesRequest.getMetadataOptions().size());
     Set<MetadataOption> metadataOptionSet = getFeaturesRequest.getMetadataOptions();
     Set<MetadataOption> expectedSet =
@@ -178,14 +396,14 @@ public class GetFeaturesRequestTest {
 
   @Test
   public void testJsonWithCustomMetadataOptions() {
-    defaultFeatureRequestData.addRequestContext("testKey", 999.99);
     getFeaturesRequest =
-        new GetFeaturesRequest(
-            TEST_WORKSPACENAME,
-            TEST_FEATURESERVICE_NAME,
-            defaultFeatureRequestData,
-            MetadataOption.NAME,
-            MetadataOption.SLO_INFO);
+        new GetFeaturesRequest.Builder()
+            .workspaceName(TEST_WORKSPACENAME)
+            .featureServiceName(TEST_FEATURESERVICE_NAME)
+            .joinKey("testKey", "testValue")
+            .requestContext("testKey", 999.99)
+            .metadataOptions(MetadataOption.NAME, MetadataOption.SLO_INFO)
+            .build();
 
     Assert.assertEquals(3, getFeaturesRequest.getMetadataOptions().size());
 
@@ -209,13 +427,14 @@ public class GetFeaturesRequestTest {
 
   @Test
   public void testJsonWithAllMetadataOptions() {
-    defaultFeatureRequestData.addRequestContext("testKey", "testValue");
     getFeaturesRequest =
-        new GetFeaturesRequest(
-            TEST_WORKSPACENAME,
-            TEST_FEATURESERVICE_NAME,
-            defaultFeatureRequestData,
-            MetadataOption.ALL);
+        new GetFeaturesRequest.Builder()
+            .workspaceName(TEST_WORKSPACENAME)
+            .featureServiceName(TEST_FEATURESERVICE_NAME)
+            .joinKey("testKey", "testValue")
+            .requestContext("testKey", "testValue")
+            .metadataOptions(MetadataOption.ALL)
+            .build();
 
     Assert.assertEquals(4, getFeaturesRequest.getMetadataOptions().size());
 
