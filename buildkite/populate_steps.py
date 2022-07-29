bcf50892d2f9b9fb7a2434fec362a259c697012e
Pooja Bhat <poojabhat@Poojas-MacBook-Pro.local>
2022-07-29 00:07:36 -0700

    Remove unwanted code

--- buildkite/populate_steps.py
+++ buildkite/populate_steps.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python3
 
 import os
-import subprocess
-
-DEFAULT_BRANCH = "main"
-
-CLIENT_GPG_KEY_SECRET_NAME = "tecton-staging/JAVA_CLIENT_GPG_KEY"
-
-
-def call(*args):
-    subprocess.check_call(args)
-
-
-def output(*args):
-    return subprocess.check_output(args).decode("utf-8")
-
-
-def get_commits_since_last(commit: str):
-    commit_count = output("git", "rev-list", "--count", f"{commit}..HEAD").strip()
-    return int(commit_count)
 
 
 def main() -> None:
