c2586f375d6b9d0ebd0aadc03b9faa3f7ca1da12
Pooja Bhat <poojabhat@Poojas-MacBook-Pro.local>
2022-08-14 10:22:15 -0700

    Fix test

--- src/test/java/ai/tecton/client/TectonClientTest.java
+++ src/test/java/ai/tecton/client/TectonClientTest.java
@@ -176,30 +176,9 @@ public class TectonClientTest {
   @Test
   public void testErrorResponseWhenJoinKeyIsMissing() {
 
-    QueueDispatcher dispatcher =
-        new QueueDispatcher() {
-          @Override
-          public MockResponse dispatch(RecordedRequest request) throws InterruptedException {
-
-            switch (request.getSequenceNumber()) {
-              case 1:
-                return new MockResponse().setResponseCode(200);
-              case 2:
-                return new MockResponse().setResponseCode(200).setBody("version=9");
-              case 3:
-                return new MockResponse()
-                    .setResponseCode(200)
-                    .setBody(
-                        "{\\\"info\\\":{\\\"name\":\"Lucas Albuquerque\",\"age\":\"21\",\"gender\":\"male\"}}");
-            }
-            return new MockResponse().setResponseCode(400);
-          }
-        };
-
     String errorResponse =
         "{\"error\":\"Missing required join key: merchant\",\"code\":3,\"message\":\"Missing required join key: merchant\"}";
 
-    mockWebServer.setDispatcher(dispatcher);
     mockWebServer.enqueue(
         new MockResponse()
             .setResponseCode(400)
