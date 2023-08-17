2a479d2cb1438b6b620ff7394797576f7b2e98d3
Pooja Bhat <poojabhat@Poojas-MacBook-Pro.local>
2023-08-17 17:55:58 -0400

    Handle Long null values

--- src/main/java/ai/tecton/client/exceptions/TectonErrorMessage.java
+++ src/main/java/ai/tecton/client/exceptions/TectonErrorMessage.java
@@ -7,7 +7,7 @@ package ai.tecton.client.exceptions;
 public class TectonErrorMessage {
   public static final String INVALID_KEY = "API Key cannot be empty";
   public static final String INVALID_URL = "Cannot connect to Tecton because the URL is invalid";
-  public static final String INVALID_KEY_VALUE = "Key/Value cannot be null or empty";
+  public static final String INVALID_KEY_VALUE = "Key cannot be null or empty.";
 
   public static final String CALL_FAILURE = "Unable to perform call. %s";
   public static final String ERROR_RESPONSE =
