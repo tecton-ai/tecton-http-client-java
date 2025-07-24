12ef437c76327ee266cd061ab63637e86f227353
Zachary Siegel <zachary.siegel@tecton.ai>
2025-07-24 16:04:29 -0400

    Fix tests

--- src/test/java/ai/tecton/client/request/RequestOptionsTest.java
+++ src/test/java/ai/tecton/client/request/RequestOptionsTest.java
@@ -13,57 +13,6 @@ public class RequestOptionsTest {
   }
 
   @Test
-  public void testIntegerOptionSetting() {
-    RequestOptions options = new RequestOptions();
-    options.setOption("latency_budget_ms", 1000);
-
-    assertEquals(Integer.valueOf(1000), options.getIntegerOption("latency_budget_ms"));
-    assertEquals(Integer.valueOf(1000), options.getOption("latency_budget_ms"));
-    assertFalse(options.isEmpty());
-    assertEquals(1, options.getOptions().size());
-  }
-
-  @Test
-  public void testBooleanOptionSetting() {
-    RequestOptions options = new RequestOptions();
-    options.setOption("coerceNullCountsToZero", true);
-
-    assertEquals(Boolean.TRUE, options.getBooleanOption("coerceNullCountsToZero"));
-    assertEquals(Boolean.TRUE, options.getOption("coerceNullCountsToZero"));
-    assertFalse(options.isEmpty());
-  }
-
-  @Test
-  public void testMultipleBooleanOptions() {
-    RequestOptions options = new RequestOptions();
-    options.setOption("readFromCache", true);
-    options.setOption("writeFromCache", false);
-    options.setOption("ignoreExtraRequestContextFields", true);
-
-    assertEquals(Boolean.TRUE, options.getBooleanOption("readFromCache"));
-    assertEquals(Boolean.FALSE, options.getBooleanOption("writeFromCache"));
-    assertEquals(Boolean.TRUE, options.getBooleanOption("ignoreExtraRequestContextFields"));
-    assertEquals(3, options.getOptions().size());
-  }
-
-  @Test
-  public void testMixedIntegerAndBooleanOptions() {
-    RequestOptions options = new RequestOptions();
-    options.setOption("latency_budget_ms", 5000);
-    options.setOption("coerceNullCountsToZero", false);
-
-    assertEquals(Integer.valueOf(5000), options.getIntegerOption("latency_budget_ms"));
-    assertEquals(Boolean.FALSE, options.getBooleanOption("coerceNullCountsToZero"));
-    assertEquals(2, options.getOptions().size());
-  }
-
-  @Test(expected = IllegalArgumentException.class)
-  public void testInvalidValueTypeThrowsException() {
-    RequestOptions options = new RequestOptions();
-    options.setOption("invalid_option", "string_value");
-  }
-
-  @Test
   public void testBuilder() {
     RequestOptions options =
         new RequestOptions.Builder()
@@ -72,9 +21,9 @@ public class RequestOptionsTest {
             .option("writeFromCache", false)
             .build();
 
-    assertEquals(Integer.valueOf(2000), options.getIntegerOption("latency_budget_ms"));
-    assertEquals(Boolean.TRUE, options.getBooleanOption("readFromCache"));
-    assertEquals(Boolean.FALSE, options.getBooleanOption("writeFromCache"));
+    assertEquals(2000, options.getOption("latency_budget_ms"));
+    assertEquals(true, options.getOption("readFromCache"));
+    assertEquals(false, options.getOption("writeFromCache"));
     assertEquals(3, options.getOptions().size());
   }
 
